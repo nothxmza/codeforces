3c30090ae38f9e1208b2b4218c3ca84190961dbf
nothxmza <77733204+nothxmza@users.noreply.github.com>
2022-08-11 19:14:51 +0200

    add cpp

---
+++ tests.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+/*designe tutorial
+int main()
+{
+	 int x = 0;
+	 int y = 0;
+	 int  z = 0;
+	 int resutl = 0;
+	 int test = 0;
+	 std::cin >> x;
+	 int i = 0;
+	if(x % 2 == 0)
+	{
+		z = 4;
+		y = x - 4;
+	}
+	else
+	{
+		z = 9;
+		y = x - 9;
+	}
+	std::cout << z  << " " << y << std::endl;
+}*/
+
+
+/*vasya the hipster
+int main()
+{
+	int a =0;
+	int b = 0;
+	int j = 0;
+	int total = 0;
+	std::cin >> a >> b;
+	int stocka= a;
+	int stockb = b;
+	while(a-- > 0 && b-- > 0)
+		j++;
+	if(stocka > stockb)
+		total = stocka - stockb;
+	else if(stockb > stocka)
+		total = stockb - stocka;
+	if(total > 0)
+	{
+		while(total %2 != 0)
+			total--;
+	}
+	if(total % 2 == 0 && total != 2)
+		total = total / 2;
+	else if(total == 2)
+		total = 1;
+	else
+		total =0;
+	std::cout <<j << " " << total << std::endl;
+}*/
+
+
+
